50983b86c263680b4870ab3a2b65b02800584152
Denghejun <hjdeng@CNhjdeng.local>
2020-11-20 15:14:13 +0800

    init

---
+++ src/main/java/org/driftprogramming/distributedlock/DistributedLockApplication.java
@@ -0,0 +1,13 @@
+package org.driftprogramming.distributedlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedLockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/driftprogramming/distributedlock/DistributedLockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.driftprogramming.distributedlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
